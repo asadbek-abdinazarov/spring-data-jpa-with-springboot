660529ba8076a0d083303262247794b333d63a9d
Asadbek <asadbekabdunazarov602@gmail.com>
2025-06-13 19:58:54 +0500

    Update

---
+++ src/main/java/uz/javachi/spring_data_jpa_with_spring_boot/SpringDataJpaWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package uz.javachi.spring_data_jpa_with_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaWithSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/javachi/spring_data_jpa_with_spring_boot/SpringDataJpaWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.javachi.spring_data_jpa_with_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
