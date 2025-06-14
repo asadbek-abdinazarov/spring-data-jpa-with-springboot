38f9c233d802f8d2c08452f957a7d58f8ba94f68
Asadbek <asadbekabdunazarov602@gmail.com>
2025-06-14 18:01:48 +0500

    jpa buddy

--- src/main/java/uz/javachi/spring_data_jpa_with_spring_boot/task/TaskRepository.java
+++
@@ -1,6 +0,0 @@
-package uz.javachi.spring_data_jpa_with_spring_boot.task;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Integer> {
-}
