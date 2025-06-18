0a75e0f303765fe00b02ef3a0fb0926aef498532
Asadbek <asadbekabdunazarov602@gmail.com>
2025-06-18 17:00:14 +0500

    async execution is learned

--- src/main/java/uz/javachi/spring_data_jpa_with_spring_boot/SimpleController.java
+++ src/main/java/uz/javachi/spring_data_jpa_with_spring_boot/SimpleController.java
@@ -1,57 +1,22 @@
 package uz.javachi.spring_data_jpa_with_spring_boot;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 public class SimpleController {
 
-    private final PersonProperties personProperties;
+    private final ReportSenderService reportSenderService;
 
-    public SimpleController(PersonProperties personProperties) {
-        this.personProperties = personProperties;
+    public SimpleController(ReportSenderService reportSenderService) {
+        this.reportSenderService = reportSenderService;
     }
 
-    @Value("${example.string:'Hello PDP'}")
-    private String string;
-
-    @Value("#{${example.numbers}}")
-    private Map<String, Integer> numbers;
-
-    @GetMapping
-    public String get() {
-        return string;
-    }
-
-    @GetMapping("/lang")
-    public List<String> languages() {
-        return personProperties.getLanguages();
-    }
-
-    @GetMapping("/lang2")
-    public List<String> languages2() {
-        return personProperties.getLanguages2();
-    }
-
-    @GetMapping("/num")
-    public Map<String, Integer> numbers() {
-        return numbers;
-    }
-
-    @GetMapping("/per")
-    public Person getPerson() {
-        return personProperties.getPerson();
-    }
-
-
-    @GetMapping("/people")
-    public List<Person> getPeoples() {
-        return personProperties.getPeoples();
+    @GetMapping("/sendReport")
+    public String sendReport() {
+        reportSenderService.sendReport("Asadbek");
+        return "Report sent successfully";
     }
 }
--- src/main/java/uz/javachi/spring_data_jpa_with_spring_boot/SpringDataJpaWithSpringBootApplication.java
+++ src/main/java/uz/javachi/spring_data_jpa_with_spring_boot/SpringDataJpaWithSpringBootApplication.java
@@ -3,9 +3,14 @@ package uz.javachi.spring_data_jpa_with_spring_boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
-@EnableConfigurationProperties(PersonProperties.class)
+@EnableAsync
 public class SpringDataJpaWithSpringBootApplication {
 
     public static void main(String[] args) {
