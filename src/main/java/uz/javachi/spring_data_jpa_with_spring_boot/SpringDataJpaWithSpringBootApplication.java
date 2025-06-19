7f9cec858378364b48b69c1d18643b897728f80d
Asadbek <asadbekabdunazarov602@gmail.com>
2025-06-19 22:29:00 +0500

    scheduling is learned

--- src/main/java/uz/javachi/spring_data_jpa_with_spring_boot/SpringDataJpaWithSpringBootApplication.java
+++ src/main/java/uz/javachi/spring_data_jpa_with_spring_boot/SpringDataJpaWithSpringBootApplication.java
@@ -6,22 +6,35 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Properties;
+import java.util.Timer;
+import java.util.TimerTask;
 
 @SpringBootApplication
 @EnableAsync
+@EnableScheduling
 public class SpringDataJpaWithSpringBootApplication {
 
+
     public static void main(String[] args) {
+       /* class ClockTask extends TimerTask {
+
+            private final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+            @Override
+            public void run() {
+                System.out.println("\r" + "Time : " + dateFormat.format(new Date()));
+            }
+        }
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new ClockTask(), 4, 2);*/
         SpringApplication.run(SpringDataJpaWithSpringBootApplication.class, args);
+
     }
 
-    /*@Bean
-    public JavaMailSender getJavaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
-        return mailSender;
-    }*/
+
 }
