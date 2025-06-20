0dad079e6fd6976ae2be1feb8b441abb26d5ffef
Asadbek <asadbekabdunazarov602@gmail.com>
2025-06-20 14:18:45 +0500

    Revert "logback is learned"
    This reverts commit 0c52ed888a00ad26b9fe4e3aa134a81a44db917c.

--- src/main/java/uz/javachi/spring_data_jpa_with_spring_boot/MailSenderController.java
+++ src/main/java/uz/javachi/spring_data_jpa_with_spring_boot/MailSenderController.java
@@ -1,55 +1,47 @@
 package uz.javachi.spring_data_jpa_with_spring_boot;
 
 import jakarta.mail.MessagingException;
-import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
-
-@Slf4j
 @RestController
 @RequestMapping("/api/mail")
 public class MailSenderController {
 
-//    private final Logger log = LoggerFactory.getLogger(MailSenderController.class);
+    private final MailSenderService mailSenderService;
 
+    public MailSenderController(MailSenderService mailSenderService) {
+        this.mailSenderService = mailSenderService;
+    }
 
     @PostMapping("/send")
     public String sendMail(@RequestParam String username) throws MessagingException {
-//        mailSenderService.sendMail(username);
-        for (int i = 0; i < 100; i++) {
-            log.info("send Sending mail to {}, Time: {}", username, new Date());
-        }
+        mailSenderService.sendMail(username);
         return "Message Sent";
     }
     @PostMapping("/html-send")
     public String sendMailHtml(@RequestParam String username) throws Exception {
-//        mailSenderService.sendMailHtml(username);
-        log.info("html-send Sending mail to {}", username);
+        mailSenderService.sendMailHtml(username);
         return "Message Sent";
     }
 
     @PostMapping("/html-send-with-free-maker")
     public String sendMailHtmlWithFreeMaker(@RequestParam String username) throws Exception {
-//        mailSenderService.sendMailHtmlWithFreeMaker(username);
-        log.info("html-send-with-free-maker Sending mail to {}", username);
+        mailSenderService.sendMailHtmlWithFreeMaker(username);
         return "Message Sent";
     }
 
     @PostMapping("/attachment-send")
     public String sendMailAttachment(@RequestParam String username) throws Exception {
-//        mailSenderService.sendMailAttachment(username);
+        mailSenderService.sendMailAttachment(username);
         return "Message Sent";
     }
 
     @PostMapping("/image-send")
     public String sendMailImage(@RequestParam String username) throws Exception {
-//        mailSenderService.sendMailImage(username);
+        mailSenderService.sendMailImage(username);
         return "Message Sent";
     }
 }
--- src/main/java/uz/javachi/spring_data_jpa_with_spring_boot/SpringDataJpaWithSpringBootApplication.java
+++ src/main/java/uz/javachi/spring_data_jpa_with_spring_boot/SpringDataJpaWithSpringBootApplication.java
@@ -2,14 +2,36 @@ package uz.javachi.spring_data_jpa_with_spring_boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Properties;
+import java.util.Timer;
+import java.util.TimerTask;
 
 @SpringBootApplication
 @EnableAsync
+@EnableScheduling
 public class SpringDataJpaWithSpringBootApplication {
 
 
     public static void main(String[] args) {
+       /* class ClockTask extends TimerTask {
+
+            private final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+            @Override
+            public void run() {
+                System.out.println("\r" + "Time : " + dateFormat.format(new Date()));
+            }
+        }
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new ClockTask(), 4, 2);*/
         SpringApplication.run(SpringDataJpaWithSpringBootApplication.class, args);
 
     }
