861d93c1b52622489c1254e16fe49936e9f2612c
Asadbek <asadbekabdunazarov602@gmail.com>
2025-06-20 14:22:10 +0500

    logback is learned

--- src/main/java/uz/javachi/spring_data_jpa_with_spring_boot/MailSenderController.java
+++ src/main/java/uz/javachi/spring_data_jpa_with_spring_boot/MailSenderController.java
@@ -1,47 +1,43 @@
 package uz.javachi.spring_data_jpa_with_spring_boot;
 
 import jakarta.mail.MessagingException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/mail")
 public class MailSenderController {
 
-    private final MailSenderService mailSenderService;
 
-    public MailSenderController(MailSenderService mailSenderService) {
-        this.mailSenderService = mailSenderService;
-    }
 
     @PostMapping("/send")
     public String sendMail(@RequestParam String username) throws MessagingException {
-        mailSenderService.sendMail(username);
+        for (int i = 0; i < 1000; i++) {
+            log.info("Sending mail to {}", username);
+        }
         return "Message Sent";
     }
     @PostMapping("/html-send")
     public String sendMailHtml(@RequestParam String username) throws Exception {
-        mailSenderService.sendMailHtml(username);
         return "Message Sent";
     }
 
     @PostMapping("/html-send-with-free-maker")
     public String sendMailHtmlWithFreeMaker(@RequestParam String username) throws Exception {
-        mailSenderService.sendMailHtmlWithFreeMaker(username);
         return "Message Sent";
     }
 
     @PostMapping("/attachment-send")
     public String sendMailAttachment(@RequestParam String username) throws Exception {
-        mailSenderService.sendMailAttachment(username);
         return "Message Sent";
     }
 
     @PostMapping("/image-send")
     public String sendMailImage(@RequestParam String username) throws Exception {
-        mailSenderService.sendMailImage(username);
         return "Message Sent";
     }
 }
